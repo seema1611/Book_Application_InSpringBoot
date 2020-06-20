29bcff0a041d0dbcda43e06300258d48c1c08ad1
seema1611 <seemarajpure16@gmail.com>
2020-06-21 01:06:36 +0530

    REFACTORED: Write a repository annotation

--- src/main/java/com/book/repository/BooksRepository.java
+++ src/main/java/com/book/repository/BooksRepository.java
@@ -2,7 +2,9 @@ package com.book.repository;
 
 import com.book.model.Books;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BooksRepository extends CrudRepository<Books, Integer> {
 
 }
